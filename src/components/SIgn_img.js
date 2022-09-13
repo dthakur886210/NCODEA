f39e510039605ae302a20bca3a2328335450e8ac
Deepak thakur <dthakur886210@gmail.com>
2022-09-13 14:31:57 +0530

    First commit

---
+++ src/components/SIgn_img.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const SIgn_img = () => {
+    return (
+        <>
+            <div className="right_data mt-5" style={{ width: "100%" }}>
+                <div className="sign_img mt-5">
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SIgn_img
